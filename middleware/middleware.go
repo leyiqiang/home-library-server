5994d10b9e0793cc845137bbf839e53110624321
leyiqiang <leyiqiang@gmail.com>
2022-01-17 17:37:52 -0600

    check token wip

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -20,3 +20,38 @@ func AdminOnly(next http.Handler) http.Handler {
 	})
 
 }
+
+// todo check Token
+//func CheckToken(next http.Handler) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		w.Header().Add("Vary", "Authorization")
+//		authHeader := r.Header.Get("Authorization")
+//
+//		if authHeader == "" {
+//
+//		}
+//
+//		headerParts := strings.Split(authHeader, " ")
+//		if len(headerParts) != 2 {
+//			utils.ErrorJSON(w, errors.New("invalid auth header"))
+//			return
+//		}
+//
+//		if headerParts[0] != "Bearer" {
+//			utils.ErrorJSON(w, errors.New("unauthorized - no bearer"))
+//			return
+//		}
+//
+//		tokenString := headerParts[1]
+//
+//		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+//			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+//				utils.ErrorJSON(w, errors.New("unexpected signing method"))
+//			}
+//
+//			//return secretmac
+//
+//		})
+//		next.ServeHTTP(w, r)
+//	})
+//}
